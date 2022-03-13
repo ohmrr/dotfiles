7fcc008de233a3876b6d9099552fabf686de1285
Ohmrrr <89240639+Ohmrrr@users.noreply.github.com>
2022-03-12 22:16:48 -0800

    Says missing a url instead of invalid when nothing is entered

--- yt-dlp/yt-song.sh
+++ yt-dlp/yt-song.sh
@@ -2,7 +2,7 @@
 
 if [ -z "$1" ]
 then
-  echo "Invalid URL"
+  echo "Enter a URL"
 else
   cd ~/Desktop && yt-dlp "$1" -x --audio-format mp3 --audio-quality 320k --embed-thumbnail --add-metadata
 fi
